4aa1eb8a45d8f4eb75c526e7a1568c894082012a
SOURABH CHOUDHARY <109725811+SOURABH7828@users.noreply.github.com>
2023-01-22 15:26:20 +0530

    first commit

---
+++ db/config.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+// mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');
+mongoose.connect('mongodb+srv://sourab:sourab@cluster0.xofopix.mongodb.net/e-commerce?retryWrites=true&w=majority')
+
+//mongoose.connect('mongodb+srv://sourabhh:sourabh@cluster0.jofm3em.mongodb.net/e-commerce?retryWrites=true&w=majority');
+// DB_URI= mongodb://sourabhh:sourabh@cluster0.jofm3em.mongodb.net/e-commerce?retryWrites=true&w=majority
